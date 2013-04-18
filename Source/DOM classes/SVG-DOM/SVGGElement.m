a58fa0aa2f548196446500e231c096459cdd5956
Peder Toftegaard Olsen <pdr@trifork.com>
2013-04-18 13:12:20 +0200

    Don't mess with the layout.

--- Source/DOM classes/SVG-DOM/SVGGElement.m
+++ Source/DOM classes/SVG-DOM/SVGGElement.m
@@ -19,34 +19,6 @@
 }
 
 - (void)layoutLayer:(CALayer *)layer {
-	CGRect mainRect = CGRectZero;
-	
-	/** Adam: make a frame thats the UNION of all sublayers frames */
-	for ( CALayer *currentLayer in [layer sublayers] )
-	{
-		CGRect subLayerFrame = currentLayer.frame;
-		mainRect = CGRectUnion(mainRect, subLayerFrame);
-	}
-	
-	layer.frame = mainRect;
-
-	/** (dont know why this is here): set each sublayer to have a frame the same size as the parent frame, but with 0 offset.
-	 
-	 if I understand this correctly, the person who wrote it should have just written:
-	 
-	 "currentLayer.bounds = layer.frame"
-	 
-	 i.e. make every layer have the same size as the parent layer.
-	 
-	 But whoever wrote this didn't document their code, so I have no idea if thats correct or not
-	 */
-	for (CALayer *currentLayer in [layer sublayers]) {
-		CGRect frame = currentLayer.frame;
-		frame.origin.x -= mainRect.origin.x;
-		frame.origin.y -= mainRect.origin.y;
-		
-		currentLayer.frame = frame;
-	}
 }
 
 @end
