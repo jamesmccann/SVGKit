ac1d266ed7ec8ad35194e6863d6bb5bfd99b7d7e
Peder Toftegaard Olsen <pdr@trifork.com>
2013-04-18 14:06:55 +0200

    SVGTextElement: Convert all uses of UIFont to CTFontRef. Added support for font-weight=bold.

--- Source/DOM classes/Unported or Partial DOM/SVGTextElement.m
+++ Source/DOM classes/Unported or Partial DOM/SVGTextElement.m
@@ -14,6 +14,11 @@
 @implementation SVGTextElement
 {
     CGPoint _currentTextPosition;
+    CTFontRef _baseFont;
+	CGFloat _baseFontAscent;
+    CGFloat _baseFontDescent;
+    CGFloat _baseFontLeading;
+    CGFloat _baseFontLineHeight;
 }
 
 @synthesize transform; // each SVGElement subclass that conforms to protocol "SVGTransformable" has to re-synthesize this to work around bugs in Apple's Objective-C 2.0 design that don't allow @properties to be extended by categories / protocols
@@ -30,29 +35,22 @@
 	 
 	 And: SVGKit works by pre-baking everything into position (its faster, and avoids Apple's broken CALayer.transform property)
 	 */
-	NSString* actualSize = [self cascadedValueForStylableProperty:@"font-size"];
-	NSString* actualFamily = [self cascadedValueForStylableProperty:@"font-family"];
-	
-	CGFloat effectiveFontSize = (actualSize.length > 0) ? [actualSize floatValue] : 12; // I chose 12. I couldn't find an official "default" value in the SVG spec.
-
-    UIFont *font = nil;
-    if (actualFamily) {
-        font = [UIFont fontWithName:actualFamily size:effectiveFontSize];
-    }
-    if (!font) {
-        font = [UIFont systemFontOfSize:effectiveFontSize];
-    }
-	
 	CGAffineTransform textTransformAbsolute = [SVGHelperUtilities transformAbsoluteIncludingViewportForTransformableOrViewportEstablishingElement:self];
 
     _currentTextPosition = CGPointMake(self.x.pixelsValue, self.y.pixelsValue);
 
+    _baseFont = [self newFontFromElement:self];
+	_baseFontAscent = CTFontGetAscent(_baseFont);
+    _baseFontDescent = CTFontGetDescent(_baseFont);
+    _baseFontLeading = CTFontGetLeading(_baseFont);
+    _baseFontLineHeight = _baseFontAscent + _baseFontDescent + _baseFontLeading;
+
     CALayer *layer = [CALayer layer];
     [SVGHelperUtilities configureCALayer:layer usingElement:self];
-    layer.bounds = CGRectMake(0, 0, 100, font.ascender); // TODO[pdr] Fix bounds when all text has been layed out
+    layer.bounds = CGRectMake(0, 0, 100, _baseFontAscent); // TODO[pdr] Fix bounds when all text has been layed out
     // layer.backgroundColor = [UIColor colorWithRed:0 green:0 blue:1 alpha:0.5].CGColor;
     layer.affineTransform = textTransformAbsolute;
-    CGSize ap = CGSizeMake(0, font.ascender/font.lineHeight);
+    CGSize ap = CGSizeMake(0, _baseFontAscent/_baseFontLineHeight);
     layer.anchorPoint = CGPointMake(ap.width, ap.height);
     layer.position = CGPointMake(0, 0);
     
@@ -81,7 +79,7 @@
                 } else {
                     didAddTrailingSpace = NO;
                 }
-                CAShapeLayer *label = [self layerWithText:text font:font];
+                CAShapeLayer *label = [self layerWithText:text font:_baseFont];
                 [SVGHelperUtilities configureCALayer:label usingElement:self];
                 [SVGHelperUtilities applyStyleToShapeLayer:label withElement:self];
                 [layer addSublayer:label];
@@ -97,16 +95,7 @@
                     if (tspanElement.y.unitType!=SVG_LENGTHTYPE_UNKNOWN) {
                         _currentTextPosition.y = tspanElement.y.pixelsValue;
                     }
-                    NSString* actualSize = [tspanElement cascadedValueForStylableProperty:@"font-size"];
-                    NSString* actualFamily = [tspanElement cascadedValueForStylableProperty:@"font-family"];
-                    CGFloat tspanFontSize = (actualSize.length > 0) ? [actualSize floatValue] : 12; // I chose 12. I couldn't find an official "default" value in the SVG spec.
-                    UIFont *tspanFont = nil;
-                    if (actualFamily) {
-                        tspanFont = [UIFont fontWithName:actualFamily size:tspanFontSize];
-                    }
-                    if (!tspanFont) {
-                        tspanFont = [UIFont systemFontOfSize:tspanFontSize];
-                    }
+                    CTFontRef tspanFont = [self newFontFromElement:tspanElement];
                     BOOL hadLeadingSpace;
                     BOOL hadTrailingSpace;
                     NSString *text = [self stripText:node.textContent hadLeadingSpace:&hadLeadingSpace hadTrailingSpace:&hadTrailingSpace];
@@ -122,12 +111,8 @@
                     CAShapeLayer *label = [self layerWithText:text font:tspanFont];
                     [SVGHelperUtilities configureCALayer:label usingElement:self];
                     [SVGHelperUtilities applyStyleToShapeLayer:label withElement:tspanElement];
-                    // Use baseline of font to align vertically
-                    CGRect b = label.bounds;
-                    b.origin.y = -font.ascender-font.descender;
-                    b.size.height = font.ascender;
-                    label.bounds = b;
                     [layer addSublayer:label];
+                    CFRelease(tspanFont);
                     // TODO[pdr] Recurse in to child elements
                 }
                 break;
@@ -139,13 +124,15 @@
             }
         }
     }
+    
+    CFRelease(_baseFont);
+    _baseFont = NULL;
 
     return [layer retain];
 }
 
 - (void)layoutLayer:(CALayer *)layer
 {
-    NSLog(@"layoutLayer:");
 }
 
 - (NSString *)stripText:(NSString *)text hadLeadingSpace:(BOOL *)hadLeadingSpace hadTrailingSpace:(BOOL *)hadTrailingSpace
@@ -169,9 +156,38 @@
     return text;
 }
 
+- (CTFontRef)newFontFromElement:(SVGElement<SVGStylable> *)element
+{
+	NSString *fontSize = [element cascadedValueForStylableProperty:@"font-size"];
+	NSString *fontFamily = [element cascadedValueForStylableProperty:@"font-family"];
+    NSString *fontWeight = [element cascadedValueForStylableProperty:@"font-weight"];
+	
+	CGFloat effectiveFontSize = (fontSize.length > 0) ? [fontSize floatValue] : 12; // I chose 12. I couldn't find an official "default" value in the SVG spec.
+
+    CTFontRef fontRef = NULL;
+    if (fontFamily) {
+        fontRef = CTFontCreateWithName((CFStringRef)fontFamily, effectiveFontSize, NULL);
+    }
+    if (!fontRef) {
+        fontRef = CTFontCreateUIFontForLanguage(kCTFontUserFontType, effectiveFontSize, NULL);
+    }
+    if (fontWeight) {
+        BOOL bold = [fontWeight isEqualToString:@"bold"];
+        if (bold) {
+            CTFontRef boldFontRef = CTFontCreateCopyWithSymbolicTraits(fontRef, effectiveFontSize, NULL, kCTFontBoldTrait, kCTFontBoldTrait);
+            if (boldFontRef) {
+                CFRelease(fontRef);
+                fontRef = boldFontRef;
+            }
+        }
+    }
+    return fontRef;
+}
+
+
 #pragma mark -
 
-- (CAShapeLayer *)layerWithText:(NSString *)text font:(UIFont *)font
+- (CAShapeLayer *)layerWithText:(NSString *)text font:(CTFontRef)font
 {
     CAShapeLayer *label = [CAShapeLayer layer];
     label.anchorPoint = CGPointZero;
@@ -182,8 +198,8 @@
     // Use font baseline for alignment
     CGRect b = textPath.bounds;
     b.origin.x = 0;
-    b.origin.y = -font.ascender-font.descender;
-    b.size.height = font.ascender;
+    b.origin.y = -_baseFontAscent+_baseFontDescent;
+    b.size.height = _baseFontAscent;
     label.bounds = b;
     //label.borderColor = [UIColor redColor].CGColor;
     //label.borderWidth = 1;
@@ -195,7 +211,7 @@
 // Requires CoreText.framework
 // This creates a graphical version of the input screen, line wrapped to the input rect.
 // Core Text involves a whole hierarchy of objects, all requiring manual management.
-- (UIBezierPath*) bezierPathWithString:(NSString*) string font:(UIFont*) font inRect:(CGRect) rect;
+- (UIBezierPath*)bezierPathWithString:(NSString*)string font:(CTFontRef)fontRef inRect:(CGRect)rect;
 {
     UIBezierPath *combinedGlyphsPath = nil;
     CGMutablePathRef combinedGlyphsPathRef = CGPathCreateMutable();
@@ -204,108 +220,95 @@
         // It would be easy to wrap the text into a different shape, including arbitrary bezier paths, if needed.
         UIBezierPath *frameShape = [UIBezierPath bezierPathWithRect:rect];
         
-        // If the font name wasn't found while creating the font object, the result is a crash.
-        // Avoid this by falling back to the system font.
-        CTFontRef fontRef;
-        if ([font fontName])
-            fontRef = CTFontCreateWithName((__bridge CFStringRef) [font fontName], [font pointSize], NULL);
-        else if (font)
-            fontRef = CTFontCreateUIFontForLanguage(kCTFontUserFontType, [font pointSize], NULL);
-        else
-            fontRef = CTFontCreateUIFontForLanguage(kCTFontUserFontType, [UIFont systemFontSize], NULL);
+        CGPoint basePoint = CGPointMake(_currentTextPosition.x, CTFontGetAscent(fontRef));
+        CFStringRef keys[] = { kCTFontAttributeName };
+        CFTypeRef values[] = { fontRef };
+        CFDictionaryRef attributesRef = CFDictionaryCreate(NULL, (const void **)&keys, (const void **)&values,
+                                                           sizeof(keys) / sizeof(keys[0]), &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
         
-        if (fontRef)
+        if (attributesRef)
         {
-            CGPoint basePoint = CGPointMake(_currentTextPosition.x, CTFontGetAscent(fontRef));
-            CFStringRef keys[] = { kCTFontAttributeName };
-            CFTypeRef values[] = { fontRef };
-            CFDictionaryRef attributesRef = CFDictionaryCreate(NULL, (const void **)&keys, (const void **)&values,
-                                                               sizeof(keys) / sizeof(keys[0]), &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+            CFAttributedStringRef attributedStringRef = CFAttributedStringCreate(NULL, (CFStringRef) string, attributesRef);
             
-            if (attributesRef)
+            if (attributedStringRef)
             {
-                CFAttributedStringRef attributedStringRef = CFAttributedStringCreate(NULL, (__bridge CFStringRef) string, attributesRef);
+                CTFramesetterRef frameSetterRef = CTFramesetterCreateWithAttributedString(attributedStringRef);
                 
-                if (attributedStringRef)
+                if (frameSetterRef)
                 {
-                    CTFramesetterRef frameSetterRef = CTFramesetterCreateWithAttributedString(attributedStringRef);
+                    CTFrameRef frameRef = CTFramesetterCreateFrame(frameSetterRef, CFRangeMake(0,0), [frameShape CGPath], NULL);
                     
-                    if (frameSetterRef)
+                    if (frameRef)
                     {
-                        CTFrameRef frameRef = CTFramesetterCreateFrame(frameSetterRef, CFRangeMake(0,0), [frameShape CGPath], NULL);
+                        CFArrayRef lines = CTFrameGetLines(frameRef);
+                        CFIndex lineCount = CFArrayGetCount(lines);
+                        CGPoint lineOrigins[lineCount];
+                        CTFrameGetLineOrigins(frameRef, CFRangeMake(0, lineCount), lineOrigins);
                         
-                        if (frameRef)
+                        for (CFIndex lineIndex = 0; lineIndex<lineCount; lineIndex++)
                         {
-                            CFArrayRef lines = CTFrameGetLines(frameRef);
-                            CFIndex lineCount = CFArrayGetCount(lines);
-                            CGPoint lineOrigins[lineCount];
-                            CTFrameGetLineOrigins(frameRef, CFRangeMake(0, lineCount), lineOrigins);
+                            CTLineRef lineRef = CFArrayGetValueAtIndex(lines, lineIndex);
+                            CGPoint lineOrigin = lineOrigins[lineIndex];
                             
-                            for (CFIndex lineIndex = 0; lineIndex<lineCount; lineIndex++)
+                            CFArrayRef runs = CTLineGetGlyphRuns(lineRef);
+                            
+                            CFIndex runCount = CFArrayGetCount(runs);
+                            for (CFIndex runIndex = 0; runIndex<runCount; runIndex++)
                             {
-                                CTLineRef lineRef = CFArrayGetValueAtIndex(lines, lineIndex);
-                                CGPoint lineOrigin = lineOrigins[lineIndex];
+                                CTRunRef runRef = CFArrayGetValueAtIndex(runs, runIndex);
                                 
-                                CFArrayRef runs = CTLineGetGlyphRuns(lineRef);
+                                CFIndex glyphCount = CTRunGetGlyphCount(runRef);
+                                CGGlyph glyphs[glyphCount];
+                                CGSize glyphAdvances[glyphCount];
+                                CGPoint glyphPositions[glyphCount];
                                 
-                                CFIndex runCount = CFArrayGetCount(runs);
-                                for (CFIndex runIndex = 0; runIndex<runCount; runIndex++)
+                                CFRange runRange = CFRangeMake(0, glyphCount);
+                                CTRunGetGlyphs(runRef, CFRangeMake(0, glyphCount), glyphs);
+                                CTRunGetPositions(runRef, runRange, glyphPositions);
+                                
+                                CTFontGetAdvancesForGlyphs(fontRef, kCTFontDefaultOrientation, glyphs, glyphAdvances, glyphCount);
+                                
+                                for (CFIndex glyphIndex = 0; glyphIndex<glyphCount; glyphIndex++)
                                 {
-                                    CTRunRef runRef = CFArrayGetValueAtIndex(runs, runIndex);
+                                    CGGlyph glyph = glyphs[glyphIndex];
                                     
-                                    CFIndex glyphCount = CTRunGetGlyphCount(runRef);
-                                    CGGlyph glyphs[glyphCount];
-                                    CGSize glyphAdvances[glyphCount];
-                                    CGPoint glyphPositions[glyphCount];
+                                    // For regular UIBezierPath drawing, we need to invert around the y axis.
+                                    CGAffineTransform glyphTransform = CGAffineTransformMakeTranslation(lineOrigin.x+glyphPositions[glyphIndex].x, rect.size.height-lineOrigin.y-glyphPositions[glyphIndex].y);
+                                    glyphTransform = CGAffineTransformScale(glyphTransform, 1, -1);
+                                    // TODO[pdr] Idea for handling rotate: glyphTransform = CGAffineTransformRotate(glyphTransform, M_PI/8);
                                     
-                                    CFRange runRange = CFRangeMake(0, glyphCount);
-                                    CTRunGetGlyphs(runRef, CFRangeMake(0, glyphCount), glyphs);
-                                    CTRunGetPositions(runRef, runRange, glyphPositions);
-                                    
-                                    CTFontGetAdvancesForGlyphs(fontRef, kCTFontDefaultOrientation, glyphs, glyphAdvances, glyphCount);
-                                    
-                                    for (CFIndex glyphIndex = 0; glyphIndex<glyphCount; glyphIndex++)
+                                    CGPathRef glyphPathRef = CTFontCreatePathForGlyph(fontRef, glyph, &glyphTransform);
+                                    if (glyphPathRef)
                                     {
-                                        CGGlyph glyph = glyphs[glyphIndex];
-                                        
-                                        // For regular UIBezierPath drawing, we need to invert around the y axis.
-                                        CGAffineTransform glyphTransform = CGAffineTransformMakeTranslation(lineOrigin.x+glyphPositions[glyphIndex].x, rect.size.height-lineOrigin.y-glyphPositions[glyphIndex].y);
-                                        glyphTransform = CGAffineTransformScale(glyphTransform, 1, -1);
-                                        // TODO[pdr] Idea for handling rotate: glyphTransform = CGAffineTransformRotate(glyphTransform, M_PI/8);
-                                        
-                                        CGPathRef glyphPathRef = CTFontCreatePathForGlyph(fontRef, glyph, &glyphTransform);
-                                        if (glyphPathRef)
-                                        {
-                                            // Finally carry out the appending.
-                                            CGPathAddPath(combinedGlyphsPathRef, NULL, glyphPathRef);
-                                            CFRelease(glyphPathRef);
-                                        }
-                                        
-                                        basePoint.x += glyphAdvances[glyphIndex].width;
-                                        basePoint.y += glyphAdvances[glyphIndex].height;
-                                        NSLog(@"'%@' => %@", [string substringWithRange:NSMakeRange(glyphIndex, 1)], NSStringFromCGPoint(basePoint));
+                                        // Finally carry out the appending.
+                                        CGPathAddPath(combinedGlyphsPathRef, NULL, glyphPathRef);
+                                        CFRelease(glyphPathRef);
                                     }
+                                    
+                                    basePoint.x += glyphAdvances[glyphIndex].width;
+                                    basePoint.y += glyphAdvances[glyphIndex].height;
+                                    NSLog(@"'%@' => %@", [string substringWithRange:NSMakeRange(glyphIndex, 1)], NSStringFromCGPoint(basePoint));
                                 }
-                                _currentTextPosition.x = basePoint.x; // TODO[pdr]
-                                // TODO[pdr] Only one line
-                                basePoint.x = 0;
-                                basePoint.y += CTFontGetAscent(fontRef) + CTFontGetDescent(fontRef) + CTFontGetLeading(fontRef);
                             }
-                            
-                            CFRelease(frameRef);
+                            _currentTextPosition.x = basePoint.x; // TODO[pdr]
+                            // TODO[pdr] Only one line
+                            basePoint.x = 0;
+                            basePoint.y += CTFontGetAscent(fontRef) + CTFontGetDescent(fontRef) + CTFontGetLeading(fontRef);
                         }
                         
-                        CFRelease(frameSetterRef);
+                        CFRelease(frameRef);
                     }
-                    CFRelease(attributedStringRef);
+                    
+                    CFRelease(frameSetterRef);
                 }
-                CFRelease(attributesRef);
+                CFRelease(attributedStringRef);
             }
-            CFRelease(fontRef);
+            CFRelease(attributesRef);
         }
+
         // Casting a CGMutablePathRef to a CGPathRef seems to be the only way to convert what was just built into a UIBezierPath.
         combinedGlyphsPath = [UIBezierPath bezierPathWithCGPath:(CGPathRef) combinedGlyphsPathRef];
-        
+    
         CGPathRelease(combinedGlyphsPathRef);
     }
     return combinedGlyphsPath;
